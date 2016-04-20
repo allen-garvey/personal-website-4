6c83b983656ab944fd731e6d071ab8144230bc54
Allen Garvey <allengarvey@hotmail.com>
2016-04-19 20:49:21 -0400

    add routing on url address change and default route to homepage when no route found

--- _includes/routing.js
+++ _includes/routing.js
@@ -4,8 +4,9 @@
 (function($){
 	var menu_headings = $('.home-menu li');
 	var home_sections = $('.home-section');
+    var homepage_text = $('#homepage-text');
     $('.home-menu li').on('click', function(event) {
-    	$('#homepage-text').hide();
+    	homepage_text.hide();
     	menu_headings.removeClass('active');
     	var clicked = $(this);
     	clicked.addClass('active');
@@ -15,9 +16,21 @@
     });
 
     //routing
-    var url = window.location.href;
-    var section_url = url.replace(/^.+#/, '');
-    if(url.match(/[#]/) && section_url){
-    	$('.home-menu li[data-section="' + section_url +'"]').click();
+    function router(){
+        var url = window.location.href;
+        var section_url = url.replace(/^.+#/, '');
+        var section_header = $('.home-menu li[data-section="' + section_url +'"]');
+        if(url.match(/[#]/) && section_url && section_header.length > 0){
+        	section_header.click();
+        }
+        else{
+            //404 - show homepage
+            home_sections.hide();
+            menu_headings.removeClass('active');
+            homepage_text.show();
+        }
     }
+    router();
+    //change route on hash url change
+    window.addEventListener("hashchange", router, false)
 })(jQuery);
