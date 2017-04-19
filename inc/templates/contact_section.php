d8b517cdb4bc6886633489649c00108104e420e4
Allen Garvey <allen.garvey@gmail.com>
2017-04-19 11:53:10 -0400

    Add link to artwork in contact section, and change contact list icons to use evil-icons

--- inc/templates/contact_section.php
+++ inc/templates/contact_section.php
@@ -3,14 +3,21 @@
 		<span class="contact-label">Email</span>
 		<a href="" id="email_link">
 			<span id="email_text"></span>
-			<span class="icon icon--email"><?php require(SVG_PATH.'icon-email.svg'); ?></span>
+			<span class="icon"><?php require(SVG_PATH.'ei-envelope.svg'); ?></span>
 		</a>
 	</li>
 	<li>
 		<span class="contact-label">GitHub</span>
 		<a href="https://github.com/allen-garvey">
-			<span class="username">allen-garvey</span>
-			<span class="icon icon--github"><?php require(SVG_PATH.'icon-github.svg'); ?></span>
+			<span>allen-garvey</span>
+			<span class="icon"><?php require(SVG_PATH.'ei-sc-github.svg'); ?></span>
+		</a>
+	</li>
+	<li>
+		<span class="contact-label">Artwork</span>
+		<a href="https://strangescenery.com">
+			<span>strangescenery.com</span>
+			<span class="icon"><?php require(SVG_PATH.'ei-image.svg'); ?></span>
 		</a>
 	</li>
 </ul>
