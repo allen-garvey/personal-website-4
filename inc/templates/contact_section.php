f40a7815ea8d367e1786e7f3fec7969fa0611e24
Allen Garvey <allen.garvey@gmail.com>
2017-04-18 23:25:26 -0400

    Add labels to contact list sections

--- inc/templates/contact_section.php
+++ inc/templates/contact_section.php
@@ -1,7 +1,16 @@
 <ul class="contact-list">
 	<li class="show-js">
-		<a href="" id="email_link"><span class="icon icon--email"><?php require(SVG_PATH.'icon-email.svg'); ?></span><span id="email_text"></span></a>
+		<span class="contact-label">Email</span>
+		<a href="" id="email_link">
+			<span id="email_text"></span>
+			<span class="icon icon--email"><?php require(SVG_PATH.'icon-email.svg'); ?></span>
+		</a>
 	</li>
-	<li><a href="https://github.com/allen-garvey"><span class="icon icon--github"><?php require(SVG_PATH.'icon-github.svg'); ?></span><span class="username">allen-garvey</span></a>
+	<li>
+		<span class="contact-label">GitHub</span>
+		<a href="https://github.com/allen-garvey">
+			<span class="username">allen-garvey</span>
+			<span class="icon icon--github"><?php require(SVG_PATH.'icon-github.svg'); ?></span>
+		</a>
 	</li>
 </ul>
