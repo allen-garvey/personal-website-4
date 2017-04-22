ad0fe7aec0d5d8ad317651317ca26944959653a4
Allen Garvey <allen.garvey@gmail.com>
2017-04-22 16:12:00 -0400

    Edit urls to be root relative

--- inc/config.php
+++ inc/config.php
@@ -11,7 +11,7 @@ define('SVG_PATH', INC_PATH.'svg/');
 define('JS_PATH', INC_PATH.'js/');
 
 //urls
-define('BASE_URL', '');
+define('BASE_URL', '/');
 define('STYLES_URL', BASE_URL);
 define('IMAGES_URL', BASE_URL.'images/');
 define('ICONS_URL', IMAGES_URL.'icons/');
