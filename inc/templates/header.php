bdebad7f5761ce2d0aff0536dcc51bb5f7468cea
Allen Garvey <allen.garvey@gmail.com>
2017-04-23 23:00:12 -0400

    Edit nav links and user profile picture to use url constants

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -1,6 +1,6 @@
 <header class="site-header">
     <a href="<?= BASE_URL ?>">
-        <img src="/images/allen-garvey.jpg" alt="Photo of Allen Garvey" class="author_img">
+        <img src="<?= IMAGES_URL ?>allen-garvey.jpg" alt="Photo of Allen Garvey" class="author_img">
     </a>
     <div class="header-text-container">
         <div class="header-text-overlay"></div>
@@ -9,9 +9,9 @@
             <p class="tagline">Full-stack developer and recovering professional bass-player</p>
             <div class="home-menu show-js">
                 <ul>
-                    <li data-section="contact"><a href="/#contact"><h2>Contact</h2></a></li>
-                    <li data-section="about"><a href="/#about"><h2>About</h2></a></li>
-                    <li data-section="projects"><a href="/#projects"><h2>Projects</h2></a></li>
+                    <li data-section="contact"><a href="<?= BASE_URL ?>#contact"><h2>Contact</h2></a></li>
+                    <li data-section="about"><a href="<?= BASE_URL ?>#about"><h2>About</h2></a></li>
+                    <li data-section="projects"><a href="<?= BASE_URL ?>#projects"><h2>Projects</h2></a></li>
                 </ul>
             </div>
         </div>
