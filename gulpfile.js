9f7e96bbaec0a670334e310ea4b92cf0a79753d8
Allen Garvey <allen.garvey@gmail.com>
2018-03-24 11:54:24 -0400

    Change gulpfile to only create minified versions of compiled js files

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,16 @@ let path = require('path');
 let gulp = require('gulp');
 let concat = require('gulp-concat');
 let uglify = require('gulp-uglify');
-let rename = require('gulp-rename');
 
 let config = require(path.join(__dirname, 'gulp-config.js'));
 
 /*
 * JavaScript Wrappers
 */
-
-function concatScriptsWrapper(files, dist_name){
+function minifyScriptsWrapper(files, dist_name){
 	return gulp.src(files)
-		.pipe(concat(`${dist_name}.js`))
-		.pipe(gulp.dest(config.js.DEST_DIR));
-}
-
-function minifyScriptsWrapper(dist_name){
-	return gulp.src(path.join(config.js.DEST_DIR, `${dist_name}.js`))
+		.pipe(concat(`${dist_name}.min.js`))
 		.pipe(uglify())
-		.pipe(rename(`${dist_name}.min.js`))
 		.pipe(gulp.dest(config.js.DEST_DIR));
 }
 
@@ -29,24 +21,15 @@ function minifyScriptsWrapper(dist_name){
 * JavaScript Tasks
 */
 //Footer js
-gulp.task('concatScripts:footer', function(){
-	return concatScriptsWrapper(config.js.app_files, config.js.DIST_NAME);
-});
-
-gulp.task('minifyScripts:footer', ['concatScripts:footer'], function(){
-	return minifyScriptsWrapper(config.js.DIST_NAME);
+gulp.task('minifyScripts:footer', function(){
+	return minifyScriptsWrapper(config.js.app_files, config.js.DIST_NAME);
 });
 
 
 //Head JS
-gulp.task('concatScripts:head', function(){
-	return concatScriptsWrapper(config.js.head_files, config.js.HEAD_DIST_NAME);
+gulp.task('minifyScripts:head', function(){
+	return minifyScriptsWrapper(config.js.head_files, config.js.HEAD_DIST_NAME);
 });
-
-gulp.task('minifyScripts:head', ['concatScripts:head'], function(){
-	return minifyScriptsWrapper(config.js.HEAD_DIST_NAME);
-});
-
 
 /*
 * Watch tasks
