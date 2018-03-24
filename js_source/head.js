039039b7a4c453c632e44de41f03a51aec01717f
Allen Garvey <allen.garvey@gmail.com>
2018-03-24 11:49:20 -0400

    Remove css night theme

--- js_source/head.js
+++ js_source/head.js
@@ -1,12 +1,5 @@
 /*
  * Initialize html element classes in head
  */
-(function(){
-	var classes = ['js'];
-	var now = new Date();
-	var currentHour = now.getHours();
-	if(currentHour >= 20 || currentHour <= 6){
-		classes.push('night');
-	}
-    document.documentElement.className = classes.join(' ');
-})();
+//add js class to document element so we know JavaScript is enabled
+document.documentElement.className = 'js';
