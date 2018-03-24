59d96a3601292e06ec43587b8b2ec48684d03f89
Allen Garvey <allen.garvey@gmail.com>
2018-03-24 11:45:06 -0400

    Fix typo in gulp-config js destination directory

--- gulp-config.js
+++ gulp-config.js
@@ -1,23 +1,23 @@
 "use strict";
 
-var config = {};
+let path = require('path');
+let config = {};
 
 
 /*
 * JavaScript configuration
 */
 config.js = {};
-config.js.SOURCE_DIR = __dirname + '/js_source/';
-config.js.DEST_DIR = __dirname + '/inc/js/';
+config.js.SOURCE_DIR = path.join(__dirname, 'js_source');
+config.js.DEST_DIR = path.join(__dirname, 'php', 'inc', 'js');
 config.js.DIST_NAME = 'app'; //name of compiled file to be served i.e. app.js and app.min.js
 config.js.HEAD_DIST_NAME = 'head';
 config.js.head_files = ['head']; //js for html head
 config.js.app_files = ['aquery', 'routing', 'email'];
 
 //add source dir prefix and .js suffix to js source files
-config.js.app_files = config.js.app_files.map(function(file){return config.js.SOURCE_DIR + file + '.js';});
-config.js.head_files = config.js.head_files.map(function(file){return config.js.SOURCE_DIR + file + '.js';});
-
+config.js.app_files = config.js.app_files.map((file)=>{return path.join(config.js.SOURCE_DIR, `${file}.js`);});
+config.js.head_files = config.js.head_files.map((file)=>{return path.join(config.js.SOURCE_DIR, `${file}.js`);});
 
 /*
 * Export config
