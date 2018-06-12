dbb64c9314b70954d3253d85b372ca25ade3f0e9
Allen Garvey <allen.garvey@gmail.com>
2018-06-12 16:38:55 -0400

    Remove svg icons from contact section

--- php/inc/config.php
+++ php/inc/config.php
@@ -7,7 +7,6 @@ define('ROOT_PATH', dirname(__FILE__, 2).'/');
 define('INC_PATH', ROOT_PATH.'inc/');
 define('TEMPLATES_PATH', INC_PATH.'templates/');
 define('MODELS_PATH', INC_PATH.'models/');
-define('SVG_PATH', INC_PATH.'svg/');
 define('JS_PATH', INC_PATH.'js/');
 
 //urls
--- php/inc/templates/contact_section.php
+++ php/inc/templates/contact_section.php
@@ -3,21 +3,18 @@
 		<span class="contact-label">Email</span>
 		<a href="" id="email_link">
 			<span id="email_text"></span>
-			<span class="icon"><?php require(SVG_PATH.'ei-envelope.svg'); ?></span>
 		</a>
 	</li>
 	<li>
 		<span class="contact-label">GitHub</span>
 		<a href="https://github.com/<?= GITHUB_USER_URL ?>">
 			<span><?= GITHUB_USER_URL ?></span>
-			<span class="icon"><?php require(SVG_PATH.'ei-sc-github.svg'); ?></span>
 		</a>
 	</li>
 	<li>
 		<span class="contact-label">Artwork</span>
 		<a href="http://strangescenery.com">
 			<span>strangescenery.com</span>
-			<span class="icon"><?php require(SVG_PATH.'ei-image.svg'); ?></span>
 		</a>
 	</li>
 </ul>
