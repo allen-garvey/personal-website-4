07aa55f38419eb259eae2bfc22bf99cdaa586ad9
Allen Garvey <allen.garvey@gmail.com>
2018-07-05 12:37:33 -0400

    Edit about section copy

--- php/inc/templates/about_text.php
+++ php/inc/templates/about_text.php
@@ -1,4 +1,4 @@
 <h2 class="home-text-title">Hi, I&#8217;m Allen.</h2>
 <p>
-	I&#8217;m a full-stack engineer that specializes in front-end development. After earning a degree in music composition and working for several years as a professional musician on cruise ships, I realized a musician&#8217;s lifestyle was not for me, and so decided to reinvent myself by going back to school to study computer science. With my background in both the arts and sciences and a commitment to getting things done, I believe I make a valuable addition to any team.
+	I&#8217;m a full-stack engineer that specializes in front-end web development. After earning a degree in music composition and working for several years as a professional musician on cruise ships, I realized a musician&#8217;s lifestyle was not for me, and so decided to reinvent myself by going back to school to study computer science. With my background in both the arts and sciences and a commitment to getting things done, I believe I can make a valuable contribution to any team.
 </p>
