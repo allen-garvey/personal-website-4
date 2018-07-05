914beb3756d39945a09feba49e13440272573e40
Allen Garvey <allen.garvey@gmail.com>
2018-07-05 12:31:00 -0400

    Simplify routing code a remove need fo aquery

--- js_source/routing.js
+++ js_source/routing.js
@@ -1,34 +1,41 @@
 /*
  * Hide and show sections based on menu title clicked
  */
-(function($){
-	var menu_headings = $('.nav li');
-	var home_sections = $('.home-section');
-    var homepage_text = $('#homepage-text');
-    menu_headings.on('click', function(event){
-        homepage_text.hide();
-    	menu_headings.removeClass('active');
-    	var clicked = $(this);
-    	clicked.addClass('active');
-    	var section_key = clicked.data('section');
-    	home_sections.hide();
-    	$("section[data-section='" + section_key + "']").show();
-    });
+(function(){
+    var navLinks = document.querySelectorAll('.nav li');
+    var contentSections = document.querySelectorAll('main.home section');
+    var sectionNames = ['about', 'contact', 'projects'];
+
+    function displaySection(sectionNameToDisplay){
+        navLinks.forEach(function(link){
+            var linkSectionName = link.attributes['data-section'].value;
+            if(linkSectionName === sectionNameToDisplay){
+                link.classList.add('active');
+            }
+            else{
+                link.classList.remove('active');
+            }
+        });
+        contentSections.forEach(function(contentSection){
+            var contentSectionName = contentSection.attributes['data-section'].value;
+            if(contentSectionName === sectionNameToDisplay){
+                contentSection.style.display = 'block';
+            }
+            else{
+                contentSection.style.display = 'none';
+            }
+        });
+    }
 
     //routing
     function router(){
         var url = window.location.href;
-        var section_url = url.replace(/^.+#/, '');
-        var section_header = $('.nav li[data-section="' + section_url +'"]');
-        if(url.match(/[#]/) && section_url && section_header.length > 0){
-        	section_header.click();
-        }
-        else{
-            //404 - show about section
-            $('.nav li[data-section="about"]').click();
-        }
+        var sectionUrl = url.replace(/^.+#/, '');
+        var sectionName = sectionNames.indexOf(sectionUrl) >= 0 ? sectionUrl : 'about';
+        displaySection(sectionName);
+
     }
     router();
     //change route on hash url change
-    window.addEventListener("hashchange", router, false)
-})(aQuery);
+    window.addEventListener("hashchange", router, false);
+})();
