64dfe003e125ab54dc33879b321b4bc511ad2472
Allen Garvey <allengarvey@hotmail.com>
2019-09-01 08:24:11 -0400

    Change to only use single quotes

--- js_source/email.js
+++ js_source/email.js
@@ -2,13 +2,13 @@
  * Email
  */
 (function(){
-	"use strict";
-    var dot = ".";
-    var at = "@";
-    var sixth = "com";
-    var second = "garvey";
-    var first = "allen";
-    var fourth = "gmail";
+	'use strict';
+    var dot = '.';
+    var at = '@';
+    var sixth = 'com';
+    var second = 'garvey';
+    var first = 'allen';
+    var fourth = 'gmail';
     var link = first + dot + second + at + fourth + dot + sixth;
     document.getElementById('email_link').href = 'mailto:' + link;
     document.getElementById('email_text').innerHTML = link;
