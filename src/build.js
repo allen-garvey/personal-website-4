da3d9454d7da57295b2497f60cffb9ca6f5acb02
Allen Garvey <allengarvey@hotmail.com>
2020-07-03 10:09:00 -0400

    Refactor handlebars compilation to reduce duplication of paths

--- src/build.js
+++ src/build.js
@@ -24,9 +24,11 @@ async function outputStyles(){
 
 async function outputIndex(){
     const handlebarsCompiler = handlebars.createCompiler();
+    const viewsPath = path.resolve(__dirname, '..', 'views');
+    const homeTemplatePath = path.resolve(viewsPath, 'home.hbs');
+    const layoutTemplatePath = path.resolve(viewsPath, 'layouts', `${handlebarsCompiler.defaultLayout}.hbs`);
+
     const homeContext = await home.getHomeContext(fs);
-    const homeTemplatePath = path.resolve(__dirname, '..', 'views', 'home.hbs');
-    const layoutTemplatePath = path.resolve(__dirname, '..', 'views', 'layouts', 'main.hbs');
     const body = await handlebarsCompiler.render(homeTemplatePath, homeContext);
     const layoutContext = Object.assign({}, homeContext, {body});
     const homeHtml = await handlebarsCompiler.render(layoutTemplatePath, layoutContext);
