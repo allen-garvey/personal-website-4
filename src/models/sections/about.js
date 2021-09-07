27c1f2076ec38264ef8ec6bebb059f86dae6c036
Allen Garvey <allengarvey@hotmail.com>
2021-09-07 08:02:46 -0400

    Edit hobbies

--- src/models/sections/about.js
+++ src/models/sections/about.js
@@ -3,6 +3,6 @@ module.exports = {
     slug: 'about',
     heading: `Hi, I'm Allen.`,
     content: {
-        body: `I specialize in front-end web development as one of my passions is how people can use computers to fuel their creativity and increase their productivity, and I believe the web is the most democratic and accessible way to do that. However my curiosity has taken me far afield and I've worked at times in full-stack web development, desktop and mobile apps, and command-line tools. Some of my other hobbies include playing and writing music, photography, chess, long walks, photography and creating digital and traditional art.`
+        body: `I specialize in front-end web development as one of my passions is how people can use computers to fuel their creativity and increase their productivity, and I believe the web is the most democratic and accessible way to do that. However my curiosity has taken me far afield and I've worked at times in full-stack web development, desktop and mobile apps, and command-line tools. Some of my other hobbies include playing and writing music, photography, chess, photography and creating digital and traditional art.`
     },
 };
