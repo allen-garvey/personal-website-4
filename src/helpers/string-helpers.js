9f29183549d1ded971a8970037bf3aa8f9a5a2ec
Allen Garvey <allengarvey@hotmail.com>
2022-02-14 07:52:06 -0500

    Fix smarten quotes helper function breaking links

--- src/helpers/string-helpers.js
+++ src/helpers/string-helpers.js
@@ -1,9 +1,18 @@
 function smartenQuotes(s){
+    const htmlAttributeStartReplacement = '<<HTML_ATTRIBUTE_START';
+    // will only work with 1 attribute per element
+    const htmlAttributeEndReplacement = '<<HTML_ATTRIBUTE_END';
+
     return s
+        // for urls
+        .replace(/="/g, htmlAttributeStartReplacement)
+        .replace(/">/g, htmlAttributeEndReplacement)
         .replace(/(\s)'/g, '$1‘')
         .replace(/'/g, '’')
         .replace(/(\s)"/g, '$1“')
-        .replace(/"/g, '”');
+        .replace(/"/g, '”')
+        .replace(new RegExp(htmlAttributeStartReplacement, 'g'), '="')
+        .replace(new RegExp(htmlAttributeEndReplacement, 'g'), '">');
 }
 
 module.exports = {
--- src/models/sections/projects.js
+++ src/models/sections/projects.js
@@ -4,14 +4,28 @@ const projects = [
         github: 'dithermark',
         demoUrl: 'https://dithermark.com',
         logo: 'dithermark-icon.svg',
-        description: 'Web application that allows you to easily create original pixel art. Includes variations on many popular image dithering algorithms, as well as original color quantization algorithm research. Uses <a href=\'https://vuejs.org/\'>Vue.js</a>, the <a href=\'https://unsplash.com/developers\'>Unsplash photo API</a> and modern JavaScript features such as: ES6 syntax, Web Workers, Canvas API, and WebGL.'
+        description: 'Web application that allows you to easily create original pixel art. Includes variations on many popular image dithering algorithms, as well as original color quantization algorithm research. Uses <a href="https://vuejs.org">Vue.js</a>, the Unsplash photo API for random images and modern JavaScript features such as: Web Workers, Canvas API, and WebGL.'
     },
     {
-        title: 'Artour',
-        github: 'artour',
+        title: 'Reason Sound Pack Viewer',
+        github: 'reason-sound-pack-viewer',
+        demoUrl: 'https://reason-packs.allengarvey.com',
+        logo: 'dithermark-icon.svg',
+        description: 'A SPA app built with Vue and Vue Router that uses an API from Reason Studios to browse and preview sound packs from their Reason+ subscription service.',
+    },
+    {
+        title: 'Chess Scout',
+        github: 'chess-scout-react',
+        demoUrl: 'https://chess-scout-react.netlify.app/',
+        logo: 'dithermark-icon.svg',
+        description: 'A SPA app that uses the Lichess API to visualize a player\'s opening repetoire and move trees. Built with React, it uses a custom PGN parser and chess rendering engine to display chess games using the browser Canvas API.',
+    },
+    {
+        title: 'Umbrella CMS',
+        github: 'phoenix-umbrella',
         demoUrl: 'https://strangescenery.com',
         logo: 'artour-icon.svg',
-        description: 'Custom CMS, blog engine and static site generator specifically designed for artwork and photography blogs. Made with <a href=\'http://elixir-lang.org/\'>Elixir</a> and the <a href=\'http://www.phoenixframework.org/\'>Phoenix Framework.</a> Features include: separate admin section, custom tags and categories, multiple image asset size support, organizing images in albums using drag-and-drop, and image carousels with optional captions and lazy asset loading.'
+        description: 'Custom CMS, blog engine and static site generator specifically designed for artwork and photography blogs. Made with <a href="https://elixir-lang.org">Elixir</a> and the <a href="https://www.phoenixframework.org">Phoenix Framework.</a> Features include: separate admin section, custom tags and categories, multiple image asset size support, organizing images in albums using drag-and-drop, and image carousels with optional captions and lazy asset loading.'
     },
     {
         title: 'Capricornus Squadron',
@@ -24,14 +38,8 @@ const projects = [
         title: 'dminjs',
         github: 'dminjs',
         logo: 'dminjs-icon.svg',
-        description: 'JavaScript minifier that strips comments and excess whitespace in the spirit of <a href=\'https://www.crockford.com/javascript/jsmin.html\'>jsmin.</a> Written in D and compiles to a native binary, which allows faster startup speed than <a href=\'https://github.com/mishoo/UglifyJS\'>UglifyJS</a> or <a href=\'https://developers.google.com/closure/compiler/\'>Google Closure Compiler.</a> Uses single-pass parsing with single character lookahead for increased speed and reduced memory usage, and supports ES5, ES2015, ES6 and ES7 versions of JavaScript syntax.'
+        description: 'JavaScript minifier that strips comments and excess whitespace in the spirit of <a href="https://www.crockford.com/jsmin.html">jsmin.</a> Written in D and compiles to a native binary, which allows faster startup speed than <a href="https://github.com/mishoo/UglifyJS">UglifyJS</a> or <a href="https://developers.google.com/closure/compiler">Google Closure Compiler.</a> Uses single-pass parsing with single character lookahead for increased speed and reduced memory usage, and supports ES5, ES2015, ES6 and ES7 versions of JavaScript syntax.'
     },
-    {
-        title: 'CSV Parser',
-        github: 'csv-parser-js',
-        logo: 'csv-parser-icon.svg',
-        description: 'CSV parser written in JavaScript that is currently being used in production as part of a corporate LMS (learning management system). Compatible with the CSV output from Excel, Google Sheets, and Apple Numbers and can be used either in the browser or on a server using <a href=\'https://nodejs.org\'>Node.js.</a> Includes unit tests using the <a href=\'https://jasmine.github.io/index.html\'>Jasmine test framework.</a>'
-    }
 ].map((project) => {
     project.githubUrl = `https://github.com/allen-garvey/${project.github}`;
     project.url = project.demoUrl || project.githubUrl;
