e0df112053dfb89a18dee9f1269b1446d5a30774
Allen Garvey <allengarvey@hotmail.com>
2022-02-28 07:53:29 -0500

    Remove word from about section

--- src/models/sections/about.js
+++ src/models/sections/about.js
@@ -3,6 +3,6 @@ module.exports = {
     slug: 'about',
     heading: `Hi, I'm Allen.`,
     content: {
-        body: `I deliver creative solutions to your problems using software. Some of my other artistic pursuits include creating digital and analog art, photography and playing and writing music.`
+        body: `I deliver creative solutions to problems using software. Some of my other artistic pursuits include creating digital and analog art, photography and playing and writing music.`
     },
 };
