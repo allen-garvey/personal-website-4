a271bf7983cd61809f4cfbf03e0bfe628f1a2a79
Allen Garvey <allengarvey@hotmail.com>
2024-11-04 11:40:53 -0500

    Change youtube thumbnails to be served locally instead of hotlinked from youtube

--- src/models/sections/videos.js
+++ src/models/sections/videos.js
@@ -2,26 +2,22 @@ const videos = [
     {
         title: 'Quest for Solitude',
         url: 'https://www.youtube.com/watch?v=24jL0av_ve8',
-        thumbnailUrl:
-            'https://i.ytimg.com/vi/24jL0av_ve8/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhaIGUoTDAP&rs=AOn4CLCO5fZ5PPgRNwAKjh1sKrJlSZtDXQ',
+        thumbnailUrl: '/images/videos/quest-for-solitude.webp',
     },
     {
         title: `A New World (No Man's Sky 2014 trailer rescore)`,
         url: 'https://www.youtube.com/watch?v=XE7q111QT4w',
-        thumbnailUrl:
-            'https://i.ytimg.com/vi/XE7q111QT4w/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhYIGUoWjAP&rs=AOn4CLAy6lh17H0hy8zRkVXd0VGbszYrFw',
+        thumbnailUrl: '/images/videos/no-mans-sky.webp',
     },
     {
         title: 'Westworld - World of dreams',
         url: 'https://www.youtube.com/watch?v=jNQIGIsRBj4',
-        thumbnailUrl:
-            'https://i.ytimg.com/vi/jNQIGIsRBj4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCBdRffpHeF47xbrJCkjVGogAGeaA',
+        thumbnailUrl: '/images/videos/westworld.webp',
     },
     {
         title: 'Last Battle - Final Fantasy XIV',
         url: 'https://www.youtube.com/watch?v=WEsaplY1yco',
-        thumbnailUrl:
-            'https://i.ytimg.com/vi/WEsaplY1yco/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB9vczLystJMziWUF_kGdyLk6doXg',
+        thumbnailUrl: '/images/videos/final-fantasy.webp',
     },
 ];
 
