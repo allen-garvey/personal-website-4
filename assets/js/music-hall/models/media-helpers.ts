3175d8e41e34723b1b1d99d1edc3b57294ecde2e
Allen Garvey <allengarvey@hotmail.com>
2024-11-04 12:36:58 -0500

    Update media helper to get track audio url from music.allengarvey.com

--- assets/js/music-hall/models/media-helpers.ts
+++ assets/js/music-hall/models/media-helpers.ts
@@ -5,18 +5,24 @@ export enum PlayState {
     IS_PLAYING = 2,
     IS_PAUSED = 3,
     IS_EMPTY = 4,
-};
+}
 
-export const areAlbumsEqual = (currentAlbum: Album | undefined, album: Album): boolean => currentAlbum?.meta.slug === album.meta.slug;
+export const areAlbumsEqual = (
+    currentAlbum: Album | undefined,
+    album: Album
+): boolean => currentAlbum?.meta.slug === album.meta.slug;
 
 const extensionForTrack = (isMp3: boolean, canPlayOpus: boolean): string => {
-    if(canPlayOpus){
+    if (canPlayOpus) {
         return 'opus';
     }
     return isMp3 ? 'mp3' : 'm4a';
 };
 
-export const mediaUrlForTrack = (track: Track, canPlayOpus: boolean): string => {
+export const mediaUrlForTrack = (
+    track: Track,
+    canPlayOpus: boolean
+): string => {
     const extension = extensionForTrack(!!track.isMp3, canPlayOpus);
-    return `/media/music/${track.filename}.${extension}`;
+    return `https://music.allengarvey.com/media/music/${track.filename}.${extension}`;
 };
