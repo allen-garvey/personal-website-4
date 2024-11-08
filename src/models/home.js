7d7887e9ee925ca71d2c89a3bf546d02fedffd2b
Allen Garvey <allengarvey@hotmail.com>
2024-11-08 16:25:22 -0500

    Remove header model as it is empty

--- src/models/home.js
+++ src/models/home.js
@@ -3,13 +3,11 @@ const webpack = require('../webpack');
 const { requireUncached } = require('../helpers/live-reload');
 
 async function getHomeContext(fs, websocketPort = null) {
-    const header = requireUncached(require.resolve('./header'));
     const sections = requireUncached(require.resolve('./sections'));
     const scriptContent = await webpack.getJs(fs);
 
     const context = {
         nav: sections.nav,
-        header,
         sections,
         stylesFilename: webpackConstants.stylesOutputFilename,
         scriptContent,
