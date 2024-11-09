39ebc9064bf818866d2faec4d07fed1159586a80
Allen Garvey <allengarvey@hotmail.com>
2024-11-09 17:36:56 -0500

    Update about section copy

--- src/models/sections/about.js
+++ src/models/sections/about.js
@@ -3,6 +3,6 @@ module.exports = {
     template: 'about',
     heading: 'Hi, I’m Allen.',
     content: {
-        body: 'I deliver creative solutions to problems using software. Some of my other interests include fine art, photography and playing and writing music.',
+        body: 'I’m a full stack web developer, with an emphasis on the front end. I love the web because its open nature means there are no gatekeepers or proprietary tools in the way of creating, publishing or viewing websites. My favorite part of software development is being able to identify a problem, brainstorm and prototype a solution, and then refine that into a finished product. Some of my other hobbies include: drawing and digital painting, playing and writing music, hiking, photography, and chess.',
     },
 };
