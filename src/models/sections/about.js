cb3c9a29944d71a1ebd41029853d704e2fac8469
Allen Garvey <allengarvey@hotmail.com>
2024-11-09 16:48:20 -0500

    Fix header text not being selectable and header author image being part of background image shadow

--- src/models/sections/about.js
+++ src/models/sections/about.js
@@ -1,8 +1,8 @@
 module.exports = {
     slug: 'about',
     template: 'about',
-    heading: `Hi, I'm Allen.`,
+    heading: 'Hi, I’m Allen.',
     content: {
-        body: `I deliver creative solutions to problems using software. Some of my other interests include fine art, photography and playing and writing music.`,
+        body: 'I deliver creative solutions to problems using software. Some of my other interests include fine art, photography and playing and writing music.',
     },
 };
