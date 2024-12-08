92792cd070c8ab369235016f072ef0a4dfebd058
Allen Garvey <allen.garvey@gmail.com>
2024-12-08 11:25:33 -0500

    Remove unused import

--- src/webpack.js
+++ src/webpack.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const webpackConstants = require('../webpack/constants');
 
 function createCompiler(fs, config) {
     const compiler = webpack(config);
