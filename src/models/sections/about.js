913e28e95e2fd87ed5b1e5a8115b5a4ed7a3a715
Allen Garvey <allengarvey@hotmail.com>
2026-06-11 13:21:35 -0400

    Update abourt section blurb

--- src/models/sections/about.js
+++ src/models/sections/about.js
@@ -3,6 +3,6 @@ module.exports = {
     template: 'about',
     heading: 'Hi, I’m Allen.',
     content: {
-        body: 'I’m a full stack web developer, with an emphasis on the front end. I love the web because its open nature means there are no gatekeepers or proprietary tools in the way of creating, publishing or viewing websites. My favorite part of software development is being able to identify a problem, brainstorm and prototype a solution, and then refine that into a finished product. Some of my other hobbies include: drawing and digital painting, playing and writing music, hiking, photography, and chess.',
+        body: 'I’m a full-stack software engineer who gets projects done, proficient across the entire delivery process: design, frontend UI, backend systems, and deployment. My personal projects and job history demonstrate that I’m highly adaptable and learn quickly, working with a variety of technologies, team structures, and company sizes. My hobbies in music and art have reinforced the value of pursuing excellence, which I strive for in my career.',
     },
 };
